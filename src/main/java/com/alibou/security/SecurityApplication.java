f140c3149b5b0841e6afcc421b406dd0101ae524
kalo <kaloyan.enev@gmail.com>
2024-03-27 19:30:16 +0100

    Configured the spring app to run on tomcat

--- src/main/java/com/alibou/security/SecurityApplication.java
+++ src/main/java/com/alibou/security/SecurityApplication.java
@@ -1,22 +1,17 @@
 package com.alibou.security;
 
-import com.alibou.security.auth.AuthenticationService;
-import com.alibou.security.auth.RegisterRequest;
 import com.alibou.security.coursesServiceController.CourseService;
 import com.alibou.security.lessons.LessonRepository;
-import com.alibou.security.user.Role;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
-import static com.alibou.security.user.Role.ADMIN;
-import static com.alibou.security.user.Role.MANAGER;
-
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
